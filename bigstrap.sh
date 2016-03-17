c194ee188885c4cae6e54ffcf2326578190fb7fe
Михаил Мальков <m.malkov@bars-open.ru>
2016-03-17 16:30:51 +0300

    Зависимости перенесены в ./requirements/, сервисы, которые можно унести в venv, унесены в venv, поправлена инструкция по установке

--- bigstrap.sh
+++ bigstrap.sh
@@ -15,22 +15,21 @@ pip install pip setuptools --upgrade
 pip install pyyaml jinja2
 
 # 3. Склонировать сервисы
-# git clone https://stash.bars-open.ru/scm/medvtr/hitsl.configurations.git configurations # Эта репа уже склонирована. Нет смысла тянуть её снова
-
 git clone https://stash.bars-open.ru/scm/medvtr/hippocrates.git -b RISAR code/hippocrates
 git clone https://stash.bars-open.ru/scm/medvtr/caesar.git -b develop code/caesar
 
-git clone https://stash.bars-open.ru/scm/medvtr/tsukino_usagi.git code/tsukino_usagi
-git clone https://stash.bars-open.ru/scm/medvtr/simplelogs.git -b feature-tsukino-usagi code/simplelogs
-git clone https://stash.bars-open.ru/scm/medvtr/nvesta.git code/nvesta
-git clone https://stash.bars-open.ru/scm/medvtr/coldstar.bouser.git code/coldstar.bouser
+# git clone https://stash.bars-open.ru/scm/medvtr/coldstar.bouser.git code/coldstar.bouser
 
 # 4. Установить зависимости
+pip install -r requirements/hippocaesar.txt
+pip install -r requirements/nvesta.txt
+pip install -r requirements/usagi.txt
+pip install -r requirements/coldstar.txt
+
 pip install git+https://github.com/hitsl/hitsl.utils.git@develop#egg=hitsl_utils
-pip install -r code/hippocrates/requirements/_base.txt
-pip install -r code/caesar/requirements/_base.txt
+pip install git+https://stash.bars-open.ru/scm/medvtr/pysimplelogs2.git@master#egg=pysimplelogs2
+pip install git+https://stash.bars-open.ru/scm/medvtr/tsukino_usagi.git@master#egg=tsukino_usagi
+pip install git+https://stash.bars-open.ru/scm/medvtr/nvesta.git@master#egg=nvesta
+pip install git+https://stash.bars-open.ru/scm/medvtr/simplelogs.git@feature-tsukino-usagi#egg=simplelogs
 
-pip install -r code/coldstar.bouser/requirements.txt
-pip install -r code/nvesta/requirements.txt
-pip install -r code/simplelogs/requirements.txt
-
+pip install -e git+https://stash.bars-open.ru/scm/medvtr/nemesis.git@develop#egg=nemesis
